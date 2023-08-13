42e376a8280acac43b49b5a3483580d9e3ce445d
JoseIgnacioTapia <josignaciot@gmail.com>
2023-08-12 21:43:36 -0300

    Middleware: Verify the role of the administrator

--- routes/usuarios.js
+++ routes/usuarios.js
@@ -3,6 +3,7 @@ const { check } = require("express-validator");
 
 const { validarCampos } = require("../middlewares/validar-campos");
 const { validarJWT } = require("../middlewares/validar-jwt");
+const { esAdminRole } = require("../middlewares/validar-roles");
 
 const {
   esRoleValido,
@@ -53,6 +54,7 @@ router.delete(
   "/:id",
   [
     validarJWT,
+    esAdminRole,
     check("id", "No es un ID válido").isMongoId(),
     check("id").custom(existeUsuarioPorId),
     validarCampos,
