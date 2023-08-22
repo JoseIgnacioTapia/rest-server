a79328e39b813cce94b4170e72d70b333f193d26
JoseIgnacioTapia <josignaciot@gmail.com>
2023-08-22 18:32:49 -0300

    Validate the extension

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -9,16 +9,28 @@ const cargarArchivo = (req, res = response) => {
   }
 
   const { archivo } = req.files;
+  const nombreCortado = archivo.name.split(".");
+  const extension = nombreCortado[nombreCortado.length - 1];
 
-  const uploadPath = path.join(__dirname, "../uploads/", archivo.name);
+  // Validar la extensión
+  const extensionesValidas = ["png", "jpg", "jpeg", "webp"];
+  if (!extensionesValidas.includes(extension)) {
+    return res.status(400).json({
+      msg: `La extensión ${extension} no es permitida, ${extensionesValidas}`,
+    });
+  }
 
-  archivo.mv(uploadPath, (err) => {
-    if (err) {
-      return res.status(500).json({ err });
-    }
+  res.json({ extension });
 
-    res.json({ msg: "File uploaded to " + uploadPath });
-  });
+  //   const uploadPath = path.join(__dirname, "../uploads/", archivo.name);
+
+  //   archivo.mv(uploadPath, (err) => {
+  //     if (err) {
+  //       return res.status(500).json({ err });
+  //     }
+
+  //     res.json({ msg: "File uploaded to " + uploadPath });
+  //   });
 };
 
 module.exports = {
