7dacad768a7451087543175982fd53062eada8e2
JoseIgnacioTapia <josignaciot@gmail.com>
2023-08-28 17:50:44 -0300

    Route to update User and Product images

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -40,10 +40,24 @@ const existeProductoPorID = async (id) => {
   }
 };
 
+// Validar colecciones permitidas
+const coleccionesPermitidas = (coleccion = "", colecciones = []) => {
+  const incluida = colecciones.includes(coleccion);
+
+  if (!incluida) {
+    throw new Error(
+      `La colección ${coleccion} no es permitida, ${colecciones}`
+    );
+  }
+
+  return true;
+};
+
 module.exports = {
   esRoleValido,
   emailExiste,
   existeUsuarioPorId,
   existeCategoriaPorID,
   existeProductoPorID,
+  coleccionesPermitidas,
 };
