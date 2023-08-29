93fb661886f8ad2f2cbe3c405144fe4149dcd0aa
JoseIgnacioTapia <josignaciot@gmail.com>
2023-08-28 21:30:30 -0300

    Validate file to upload

--- routes/uploads.js
+++ routes/uploads.js
@@ -1,17 +1,18 @@
 const { Router } = require("express");
 const { check } = require("express-validator");
 
-const { validarCampos } = require("../middlewares/validar-campos");
+const { validarCampos, validarArchivoSubir } = require("../middlewares/index");
 const { cargarArchivo, actualizarImagen } = require("../controllers/uploads");
 const { coleccionesPermitidas } = require("../helpers");
 
 const router = Router();
 
-router.post("/", cargarArchivo);
+router.post("/", validarArchivoSubir, cargarArchivo);
 
 router.put(
   "/:coleccion/:id",
   [
+    validarArchivoSubir,
     check("id", "El id debe ser de mongo").isMongoId(),
     check("coleccion").custom((c) =>
       coleccionesPermitidas(c, ["usuarios", "productos"])
