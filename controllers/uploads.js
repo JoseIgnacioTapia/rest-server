dc738d11306b7ae1bf1c08eb6c51245a4c266a47
JoseIgnacioTapia <josignaciot@gmail.com>
2023-08-31 18:15:51 -0300

    Delete Cloudinary images

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -103,6 +103,10 @@ const actualizarImagenCloudinary = async (req, res = response) => {
 
   // Limpiar imágenes previas
   if (modelo.img) {
+    const nombreArr = modelo.img.split("/");
+    const nombre = nombreArr[nombreArr.length - 1];
+    const [public_id] = nombre.split(".");
+    cloudinary.uploader.destroy(public_id);
   }
 
   const { tempFilePath } = req.files.archivo;
